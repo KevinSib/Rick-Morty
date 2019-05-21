283aa629869589588affb4e8b8160e0f5f9015d1
KevSib <ksibue@gmail.com>
2019-05-21 12:13:12 +0200

    create cell for character; fragment layout and improve some classes

--- app/src/main/java/com/ynov/kotlin/rickmorty/presentation/MainActivity.kt
+++ app/src/main/java/com/ynov/kotlin/rickmorty/presentation/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
     val apiManager : ApiManager = ApiManager()
         var characterResult :Single<CharacterResult> = apiManager.RetrieveCharacters()
 
--- app/src/main/java/com/ynov/kotlin/rickmorty/presentation/fragments/CharactersFragment.kt
+++ app/src/main/java/com/ynov/kotlin/rickmorty/presentation/fragments/CharactersFragment.kt
@@ -5,11 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.ynov.kotlin.rickmorty.R
+import com.ynov.kotlin.rickmorty.data.entity.Character
+import com.ynov.kotlin.rickmorty.presentation.adapters.BaseRecyclerViewAdapter.IRecyclerViewManager
+import com.ynov.kotlin.rickmorty.presentation.adapters.CharactersRecyclerViewAdapters
+import com.ynov.kotlin.rickmorty.presentation.viewHolders.BaseViewHolder
 
 
-class CharactersFragment : Fragment() {
+class CharactersFragment : Fragment(), IRecyclerViewManager, BaseViewHolder.IItemOnClickListener {
+
+    var mItems: MutableList<Character> = mutableListOf()
+    var mRecyclerView: RecyclerView? = null
+
+    override val onClickListenerManager: BaseViewHolder.IItemOnClickListener
+        get() = this
+
+    override val items: MutableList<Any>
+        get() = mItems as MutableList<Any>
+
+    //region Default Methods
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +36,45 @@ class CharactersFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+        val view = inflater.inflate(R.layout.fragment_characters, container, false)
+        mRecyclerView = view.findViewById<RecyclerView>(R.id.fragment_characters_recyclerview)
+        return view
+    }
+
+    override fun onStart() {
+
+        super.onStart()
+
+        val adapter = CharactersRecyclerViewAdapters()
+        adapter.manager = this
+
+        mRecyclerView?.let {
+            it.layoutManager = GridLayoutManager(context, 3)
+            it.adapter = adapter
+        }
+
     }
 
     companion object {
         @JvmStatic
         fun newInstance() = CharactersFragment()
     }
+
+    //endregion
+
+    //region RecyclerView Manager Methods
+
+    override fun numberOfItem(): Int = items.size
+
+    override fun getItemAtPosition(position: Int): Any = items.get(position)
+
+    //endregion
+
+    //region ItemClick Delegate Methods
+
+    override fun OnClickRecyclerViewItem(obj: Any, atPosition: Int) {
+        //  TODO React to item clicks
+    }
+
+    //endregion
 }
