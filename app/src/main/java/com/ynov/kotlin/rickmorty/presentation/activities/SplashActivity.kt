f715371f87720a288a91a69e0f8d05b7def807bc
KevSib <ksibue@gmail.com>
2019-05-22 17:17:27 +0200

    minor update

---
+++ app/src/main/java/com/ynov/kotlin/rickmorty/presentation/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.ynov.kotlin.rickmorty.presentation.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
